ba3a2782d7d27c293f573de0204b5fe53293dc63
angie9208 <avbarajasc@unal.edu.co>
2020-11-23 11:39:02 -0500

    fcambio1

---
+++ server_api/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define( 'DB_NAME', 'embebidos');
+define( 'DB_USER', 'emb2020');
+define( 'DB_PASSWORD', 'EmbebidoS');
+define( 'DB_HOST', 'aulal.org');
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+?>
